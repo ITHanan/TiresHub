19c0db20538908343aa1bd1ca7b4a78525734a78
ITHanan <155847905+ITHanan@users.noreply.github.com>
2026-02-02 23:56:06 +0100

    localhost front end changed

--- ApiLayer/Program.cs
+++ ApiLayer/Program.cs
@@ -22,7 +22,7 @@ namespace ApiLayer
                 options.AddPolicy("DevCors", policy =>
                 {
                     policy
-                        .WithOrigins("http://localhost:8080")
+                        .WithOrigins("http://localhost:8084")
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
